80a0c88e57454cf2c0d93db4ff8f2959c8af8d73
Minh Bui <tranduyminh.bui@gmail.com>
2020-09-06 19:55:52 +1000

    try todoApp

--- App.js
+++ App.js
@@ -3,15 +3,37 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Mih here');
+  const [goals, setGoals] = useState([]);
+  const [goal, setGoal] = useState('');
 
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
       <View style={styles.inputContainer}>
-        <Text style={styles.text}>{outputText}</Text>
-        <TextInput style={styles.textInput} placeholder='my goals' />
-        <Button title='ADD' onPress={() => setOutputText('Hi there!!!')} />
+        {/* <Text style={styles.text}>{outputText}</Text> */}
+        <TextInput
+          style={styles.textInput}
+          placeholder='my goals'
+          value={goal}
+          onChangeText={(text) => setGoal(text)}
+        />
+        <Button title='ADD' onPress={() => setGoals([...goals, goal])} />
+      </View>
+      <View style={styles.inputContainer}>
+        {goals
+          ? goals.map((goal, i) => (
+              <Text
+                key={i}
+                style={styles.text}
+                onPress={() => {
+                  let newGoals = goals.filter((g) => g !== goal);
+                  setGoals(newGoals);
+                }}
+              >
+                {goal}
+              </Text>
+            ))
+          : null}
       </View>
     </View>
   );
@@ -20,12 +42,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   inputContainer: {
-    flexDirection: 'row',
+    flexDirection: 'column',
   },
   text: {
     color: '#322',
